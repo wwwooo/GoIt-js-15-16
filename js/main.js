fd56beb4b9ad60950fab368b93b38d4f380a3d58
wwwooo <malinovsvska@gmail.com>
2016-10-04 20:25:12 +0300

    update main.js

--- js/main.js
+++ js/main.js
@@ -4,24 +4,22 @@ document.addEventListener('DOMContentLoaded', function() {
     var content = document.getElementById('content');
     var request = new XMLHttpRequest();
 
-    var get_json = function(tag, counter) {
-        counter = counter || 15;
-        request.open('GET', 'https://api.riffsy.com/v1/search?key=LIVDSRZULELA&tag=' + tag + '&limit='+ counter);
+    var makeRequest = function(tag, callback, limit) {
+        limit = limit || 15;
+        request.open('GET', 'https://api.riffsy.com/v1/search?key=LIVDSRZULELA&tag=' + tag + '&limit=' + limit);
         request.onreadystatechange = function() {
             if (request.status === 200 && request.readyState === 4) {
-                search.value = tag;
-                var data = JSON.parse(request.responseText);
-                var html = document.getElementById('item_tmpl').innerHTML;
-                content.insertAdjacentHTML('beforeEnd', tmpl(html, data));
+                callback(JSON.parse(request.responseText));
             } else if (request.status !== 200) {
-                console.log('false');
+                console.log('false request');
             }
         };
         request.send();
     };
 
     submit.addEventListener('click', function() {
-        content.innerHTML = '';
-        get_json(search.value);
+        makeRequest(search.value, function(data) {
+            content.innerHTML = tmpl('item_tmpl', data);
+        });
     });
 });
